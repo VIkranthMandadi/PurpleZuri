f700a1b29681d1fb83f4e55a6ba7abd4e92cc5a7
vikranth <vikmandadi@gmail.com>
2026-01-05 18:18:07 -0800

    change project name

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('learnAngular');
+  protected readonly title = signal('Purple Zuri');
 }
